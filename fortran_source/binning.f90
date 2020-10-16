00a85e01fa64be2564c53ece82907416a217e0f8
Matthias Pickem <matthias.pickem@gmail.com>
2020-10-16 16:23:31 +0200

    Added complex version of the same subroutine

--- fortran_source/binning.f90
+++ fortran_source/binning.f90
@@ -14,7 +14,7 @@ module Mbinning
   ! the matrix, vector sizes go to the end of the call list
   ! since we do not have to provide them in python
   ! automatic detection
-  subroutine binMatrix(inMatrix, vec1, vec2, outMatrix, inX, inY, vec1length, vec2length, outX, outY)
+  subroutine binMatrix_r(inMatrix, vec1, vec2, outMatrix, inX, inY, vec1length, vec2length, outX, outY)
     implicit none
     integer, intent(in) :: inX, inY, vec1length, vec2length, outX, outY
 
@@ -29,6 +29,102 @@ module Mbinning
     integer :: i,j
     integer :: ii,jj
     real(dp) :: tempsum
+
+    integer :: xbegin, xend
+    integer :: ybegin, yend
+
+    ! check vector lengths
+
+    if ((vec1length+1 /= outX) .or. (vec2length+1 /= outY)) then
+      write(*,*) 'WARNING: outMatrix size inconsistent with vector'
+    endif
+
+
+    ordered = .true.
+    do i=2,vec1length
+      if (vec1(i) <= vec1(i-1)) then
+        ordered = .false.
+      endif
+    enddo
+
+    if (.not. ordered) then
+      write(*,*) 'WARNING: X-vector not ordered'
+    endif
+
+    ordered = .true.
+    do j=2,vec2length
+      if (vec2(j) <= vec2(j-1)) then
+        ordered = .false.
+      endif
+    enddo
+
+    if (.not. ordered) then
+      write(*,*) 'WARNING: Y-vector not ordered'
+    endif
+
+
+    ! we scan through the coarse grid
+    do j=1,vec2length+1
+      do i=1,vec1length+1
+
+        ! determine index ranges
+        ! here we go from positions [one-indexed]
+        ! to intervals
+        if (i==1) then
+          xbegin = 1
+        else
+          xbegin = vec1(i-1)+1
+        endif
+
+        if (i==(vec1length+1)) then
+          xend = inX
+        else
+          xend = vec1(i)
+        endif
+
+        if (j==1) then
+          ybegin = 1
+        else
+          ybegin = vec2(j-1)+1
+        endif
+
+        if (j==(vec2length+1)) then
+          yend = inY
+        else
+          yend = vec2(j)
+        endif
+
+
+        ! index through the coarse grid and sum all elements
+        tempsum = 0
+        do jj=ybegin,yend
+          do ii=xbegin,xend
+            tempsum = tempsum + inMatrix(ii,jj)
+          enddo
+        enddo
+
+        outMatrix(i,j) = tempsum
+
+      enddo
+    enddo
+
+  end subroutine
+
+  subroutine binMatrix_c(inMatrix, vec1, vec2, outMatrix, inX, inY, vec1length, vec2length, outX, outY)
+    implicit none
+    integer, intent(in) :: inX, inY, vec1length, vec2length, outX, outY
+
+    complex(dp), dimension(inX, inY), intent(in) :: inMatrix
+    complex(dp), dimension(outX, outY), intent(inout) :: outMatrix
+
+    integer, dimension(vec1length), intent(in) :: vec1
+    integer, dimension(vec2length), intent(in) :: vec2
+
+    logical :: ordered
+
+    integer :: i,j
+    integer :: ii,jj
+    complex(dp) :: tempsum
 
     integer :: xbegin, xend
     integer :: ybegin, yend
--- main.py
+++ main.py
@@ -14,15 +14,14 @@ print(mbinning.test(3,5))
 
 # fortran ordering is important
 # dont know if one can avoid this
-A = np.ones((5000,5000), dtype=np.float64, order='F')
-
+A = np.ones((5000,5000), dtype=np.complex128, order='F')
 
 vec1 = np.array([1000,], order='F', dtype=np.int)
 vec2 = np.array([1500,3000], order='F', dtype=np.int)
 
-B = np.zeros((vec1.shape[0]+1, vec2.shape[0]+1), dtype=np.float64, order='F')
+B = np.zeros((vec1.shape[0]+1, vec2.shape[0]+1), dtype=np.complex128, order='F')
 
-mbinning.binmatrix(A,vec1,vec2,B)
+mbinning.binmatrix_c(A,vec1,vec2,B)
 
 print(A)
 print(B)
